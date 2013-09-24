1b23ce532fe870e2cbadc1c5efb0a32e649bedf9
Lasse Bergström <lasse@nextit.se>
2013-09-24 20:39:35 +0200

    Refactored test

--- src/test/java/com/mytutorial/webservice/client/HelloUserWebServiceImplTest.java
+++ src/test/java/com/mytutorial/webservice/client/HelloUserWebServiceImplTest.java
@@ -15,98 +15,50 @@ import javax.ws.rs.core.UriBuilder;
 import java.io.StringWriter;
 import java.net.URI;
 
+import static com.sun.jersey.api.client.Client.*;
 import static javax.ws.rs.core.MediaType.APPLICATION_JSON;
 import static javax.ws.rs.core.MediaType.APPLICATION_XML;
 import static org.junit.Assert.assertEquals;
 
 public class HelloUserWebServiceImplTest {
-    Client client = Client.create();
-
-    @Before
-    public void setup() {
-    }
-
-    @Test
-    @Ignore
-    public void postJsonTrackToMetallicaService() {
-        Client client = Client.create();
-        WebResource webResource = client.resource("http://localhost:8080/RESTfulExample/rest/json/metallica/post");
-        String input = "{\"singer\":\"Metallica\",\"title\":\"Fade To Black\"}";
-        ClientResponse response = webResource.type(APPLICATION_JSON).post(ClientResponse.class, input);
-
-        if (response.getStatus() != 201) {
-            throw new RuntimeException("Failed : HTTP error code : "
-                    + response.getStatus());
-        }
-
-        System.out.println("Output from Server .... \n");
-        String output = response.getEntity(String.class);
-        System.out.println(output);
-    }
+    Client client = create();
+    WebResource webResource;
+    Gson gson = new Gson();
 
     @Test
     public void postJsonTrackToUserService() {
-        Client client = Client.create();
-        WebResource webResource = client.resource("http://localhost:8080/userservice/track");
-//        String input = "{\"singer\":\"Metallica\",\"title\":\"Fade To Black\"}";
-        Gson gson = new Gson();
+        webResource = client.resource("http://localhost:8080/userservice/track");
         Track t = new Track();
         t.setSinger("Metallica");
         t.setTitle("Fade to Black");
-        String json = gson.toJson(t);
-        ClientResponse response = webResource.type(APPLICATION_JSON).post(ClientResponse.class, json);
-        if (response.getStatus() != 201) {
-            throw new RuntimeException("Failed : HTTP error code : "
-                    + response.getStatus());
-        }
+        ClientResponse response = webResource.type(APPLICATION_JSON).post(ClientResponse.class, gson.toJson(t));
 
         assertEquals("POST Track updated : Track [title=Fade to Black, singer=Metallica]", response.getEntity(String.class));
     }
 
     @Test
     public void putJsonTrackToUserService() {
-        Client client = Client.create();
-        WebResource webResource = client.resource("http://localhost:8080/userservice/track");
+        webResource = client.resource("http://localhost:8080/userservice/track");
         String input = "{\"singer\":\"Metallica\",\"title\":\"Fade To Black\"}";
         ClientResponse response = webResource.type(APPLICATION_JSON).put(ClientResponse.class, input);
-        if (response.getStatus() != 201) {
-            throw new RuntimeException("Failed : HTTP error code : "
-                    + response.getStatus());
-        }
 
         assertEquals("PUT Track created : Track [title=Fade To Black, singer=Metallica]", response.getEntity(String.class));
     }
 
     @Test
     public void putJsonUserToUserService() {
-        Client client = Client.create();
-        WebResource webResource = client.resource("http://localhost:8080/userservice/user");
-//        String input = "{\"name\":\"Ubbe\"}";
-        Gson gson = new Gson();
+        webResource = client.resource("http://localhost:8080/userservice/user");
         Customer customer = new Customer("UbbaPut");
-        String json = gson.toJson(customer);
-        ClientResponse response = webResource.type(APPLICATION_JSON).put(ClientResponse.class, json);
-        if (response.getStatus() != 201) {
-            throw new RuntimeException("Failed : HTTP error code : "
-                    + response.getStatus());
-        }
+        ClientResponse response = webResource.type(APPLICATION_JSON).put(ClientResponse.class, gson.toJson(customer));
 
         assertEquals("PUT User created : UbbaPut", response.getEntity(String.class));
     }
 
     @Test
     public void postJsonUserToUserService() {
-        Client client = Client.create();
-        WebResource webResource = client.resource("http://localhost:8080/userservice/user");
-//        String input = "{\"name\":\"Ubbe\"}";
-        Gson gson = new Gson();
+        webResource = client.resource("http://localhost:8080/userservice/user");
         Customer customer = new Customer("UbbaPost");
-        String json = gson.toJson(customer);
-        ClientResponse response = webResource.type(APPLICATION_JSON).post(ClientResponse.class, json);
-        if (response.getStatus() != 201) {
-            throw new RuntimeException("Failed : HTTP error code : "
-                    + response.getStatus());
-        }
+        ClientResponse response = webResource.type(APPLICATION_JSON).post(ClientResponse.class, gson.toJson(customer));
 
         assertEquals("POST User updated : UbbaPost", response.getEntity(String.class));
     }
@@ -120,11 +72,25 @@ public class HelloUserWebServiceImplTest {
         String xml = writer.toString();
         WebResource webResource = client.resource("http://localhost:8080/userservice/user");
         ClientResponse response = webResource.type(APPLICATION_XML).put(ClientResponse.class, xml);
-        if (response.getStatus() != 201) {
-            throw new RuntimeException("Failed : HTTP error code : " + response.getStatus());
-        }
 
         assertEquals("PUT User created : Urban", response.getEntity(String.class));
+    }
+
+    @Test
+    @Ignore
+    public void postJsonTrackToMetallicaService() {
+        WebResource webResource = client.resource("http://localhost:8080/RESTfulExample/rest/json/metallica/post");
+        String input = "{\"singer\":\"Metallica\",\"title\":\"Fade To Black\"}";
+        ClientResponse response = webResource.type(APPLICATION_JSON).post(ClientResponse.class, input);
+
+        if (response.getStatus() != 201) {
+            throw new RuntimeException("Failed : HTTP error code : "
+                    + response.getStatus());
+        }
+
+        System.out.println("Output from Server .... \n");
+        String output = response.getEntity(String.class);
+        System.out.println(output);
     }
 
     private void getXml(Client client) {
